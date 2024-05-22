82a515bbe2f7dce5e1370d9a44765deb8d17c3ad
KevalSardhara <kevalsardhara9@gmail.com>
2024-05-23 00:20:12 +0530

    first commit

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.get('/', async function (req, res, next) {
     //   {}
     // ]);
     
-    console.log("add data");
+    console.log("add data IT1");
     
     return res.status(200).json({
       status: true,
