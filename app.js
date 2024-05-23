c8faec82ce458913c333d8ec709e742c7d051d33
KevalSardhara <kevalsardhara9@gmail.com>
2024-05-24 00:28:08 +0530

    changes

--- app.js
+++ app.js
@@ -77,8 +77,6 @@ app.get('/', async function (req, res, next) {
     console.log("IT11111 add here");
     console.log("IT11111 add here");
     console.log("IT11111 add here");
-    console.log("IT11111 add here");
-    console.log("IT11111 add here");
 
     return res.status(200).json({
       status: true,
