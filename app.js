af19ddd87a705f69f158f30a8ade58c8ae024ab9
KevalSardhara <kevalsardhara9@gmail.com>
2024-05-23 23:37:45 +0530

    commit

--- app.js
+++ app.js
@@ -74,11 +74,6 @@ app.get('/', async function (req, res, next) {
     // ]);
     
 
-    console.log("CI/CD pipelines");
-    console.log("Add");
-    console.log("IT6 here");
-
-    console.log("IT2");
     console.log("IT2 add here");
     return res.status(200).json({
       status: true,
