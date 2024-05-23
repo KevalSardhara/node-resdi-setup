bc88fb1e8b225114bc4551f108dd0af25d8dd75b
KevalSardhara <96347673+KevalSardhara@users.noreply.github.com>
2024-05-23 23:01:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -76,7 +76,6 @@ app.get('/', async function (req, res, next) {
 
     console.log("CI/CD pipelines");
     console.log("Add");
-    console.log("IT6 here");
     return res.status(200).json({
       status: true,
       data: redisData
